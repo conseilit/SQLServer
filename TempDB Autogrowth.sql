bf1b4e84bc1e33dbf7e84c87e4eee30ff8a9d09a
Christophe Laporte <35958968+conseilit@users.noreply.github.com>
2022-09-27 10:21:30 +0200

    Update TempDB Autogrowth.sql

--- TempDB Autogrowth.sql
+++ TempDB Autogrowth.sql
@@ -4,7 +4,7 @@ CREATE EVENT SESSION [TempDBAutogrowth] ON SERVER
         ADD EVENT sqlserver.database_file_size_change(
             ACTION(sqlserver.client_app_name,sqlserver.client_hostname,sqlserver.database_id,sqlserver.database_name,sqlserver.session_id,sqlserver.sql_text)
             WHERE ([database_id]=(2) AND [session_id]>(50))),
-        ADD EVENT sqlserver.databases_log_file_used_size_changed(
+        ADD EVENT sqlserver.databases_log_file_used_changed(
             ACTION(sqlserver.client_app_name,sqlserver.client_hostname,sqlserver.database_id,sqlserver.database_name,sqlserver.session_id,sqlserver.sql_text)
             WHERE ([database_id]=(2) AND [session_id]>(50)))
         ADD TARGET package0.event_file(SET filename=N'TempDBAutogrowth',max_file_size=(50),max_rollover_files=(10))
